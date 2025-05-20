757132c749357fc06f0b7c162ea24934d867986c
Wagner <souza.wagner@escolar.ifrn.edu.br>
2025-05-20 18:36:41 -0300

    Segundo form

--- cadastro/script.js
+++ cadastro/script.js
@@ -1,8 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
-    const passwordInput = document.getElementById("password");
-    const submitButton = document.querySelector(".submit-button");
-  
+  const form = document.querySelector("form");
+  const passwordInput = document.getElementById("password");
+  const submitButton = document.querySelector(".submit-button");
+  const publicar = document.querySelector(".publish");
+  const ouvir = document.querySelector(".hear");
+  const cpf = document.getElementById("cpf");
+  const pix = document.getElementById("pix");
+  const email = document.getElementById("email");
+  const ancora1 = document.querySelector(".container1");
+  const ancora2 = document.querySelector(".container2");
+  const formulario1 = document.querySelector(".container-form1");
+  const formulario2 = document.querySelector(".container-form2");
+  const nomeEntrar = document.querySelector(".enter-name");
+
+
     // Critérios de validação
     const criteria = {
       letter: {
@@ -39,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   
       // Fora do forEach: ativa/desativa botão
-      if (isPasswordValid(value)) {
+      if (isPasswordValid(value) && email.checkValidity()) {
         submitButton.disabled = false;
         submitButton.classList.remove("disabled");
       } else {
@@ -47,17 +58,42 @@ document.addEventListener("DOMContentLoaded", function () {
         submitButton.classList.add("disabled");
       }
     }
-  
+
+    publicar.addEventListener("click", function () {
+      document.getElementById("input-container-publish").style.display = "block";
+      cpf.required = true;
+      pix.required = true;
+    });
+
+    ouvir.addEventListener("click", function(){
+      document.getElementById("input-container-publish").style.display = "none";
+      pix.required = false;
+      cpf.required = false;
+    });
+
     // Escuta digitação no input de senha
     passwordInput.addEventListener("input", function () {
       validatePassword(passwordInput.value);
     });
-  
+
+    email.addEventListener("input", function () {
+      validatePassword(passwordInput.value);
+    });
 
     form.addEventListener("submit", function (event) {
         if (!isPasswordValid(passwordInput.value)) {
             event.preventDefault();
         }
+    });
+
+
+    submitButton.addEventListener("click", function (event) {
+      event.preventDefault(); // <-- PREVINE o envio do formulário e recarregamento
+      ancora1.style.display = "none";
+      ancora2.style.display = "block";
+      nomeEntrar.style.visibility = "hidden";
+      formulario1.style.display = "none";
+      formulario2.style.display = "block";
     });
 
   
